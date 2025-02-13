fdc4fb571040d413b81273c61c26f836c2a5a80b
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-13 00:09:24 -0800

    fixed backend errors

--- server/app.js
+++ server/app.js
@@ -3,20 +3,21 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authroutes.js';
 import dotenv from 'dotenv';
-const PORT = process.env.PORT || 3000;
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
-  app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
-
