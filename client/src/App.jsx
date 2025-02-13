026710772d50f0c2224214cd4b97d1c239d0691c
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-12 23:40:45 -0800

    basic component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import {BrowserRouter, Route, Routes } from 'react-router';
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <h1>hello</h1>
+    <div>
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      
+    </div>
   )
 }
 
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import Login from '../Login/Login'
+import Logout from '../Logout/Logout'
+export default function Navbar() {
+  return (
+    <>
+    <div>Navbar</div>
+    <Login />
+    <Logout />
+    </>
+  )
+}
