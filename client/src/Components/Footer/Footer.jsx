4210090817eacf4a683d57c89544d4088a7896c8
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 12:28:47 -0800

    footer addition

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+        {/* Copyright Text */}
+        <p className='copyright'>Copyright &copy; 2025 NodeForum. All rights reserved.</p>
+    </footer>
+  )
+}
