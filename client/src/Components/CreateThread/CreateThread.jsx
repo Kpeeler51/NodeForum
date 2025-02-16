16e33326bdc6d5534b0019ef6489797ad9ad3108
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 13:25:53 -0800

    removed forgotten import from createthread

--- client/src/Components/CreateThread/CreateThread.jsx
+++ client/src/Components/CreateThread/CreateThread.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import PropTypes from 'prop-types';
-import './CreateThread.css';
 import storage from '../../utils/storage';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
