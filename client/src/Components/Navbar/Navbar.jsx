f3bd66e624f628095a840ae95e6e4f31a9f01e15
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 17:54:25 -0800

    Fixed naming error in navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import './Navbar.css';
 
-const Nav = ({ isAuthenticated, onLogout }) => {
+const Navbar = ({ isAuthenticated, onLogout }) => {
     return (
         <nav className='navbar'>
             <div className='navbar-left'>
@@ -31,9 +31,9 @@ const Nav = ({ isAuthenticated, onLogout }) => {
     );
 };
 
-Nav.propTypes = {
+Navbar.propTypes = {
     isAuthenticated: PropTypes.bool.isRequired,
     onLogout: PropTypes.func.isRequired
 };
 
-export default Nav;
+export default Navbar;
