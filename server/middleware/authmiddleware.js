5db52dad0a602c0e740f3f8b93d5dbfe94f6b665
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 19:20:47 -0800

    authmiddleware comments

--- server/middleware/authmiddleware.js
+++ server/middleware/authmiddleware.js
@@ -1,22 +1,32 @@
 import jwt from 'jsonwebtoken';
 
+// Middleware to authenticate JWT tokens
 export const authenticateToken = (req, res, next) => {
+    // Extract the Authorization header
     const authHeader = req.headers['authorization'];
-    console.log('Auth Header:', authHeader);
 
+    // Extract the token from the Authorization header
     const token = authHeader && authHeader.split(' ')[1];
-    console.log('Extracted Token:', token);
 
+    // If no token is provided, return 401 Unauthorized
     if (token == null) {
         console.log('No token provided');
         return res.sendStatus(401);
     }
 
     try {
+        // Verify the token using the JWT_SECRET
+        // If verification is successful, it returns the decoded token payload
         const user = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Attach the user information to the request object
         req.user = user;
+
+        // Proceed to the next middleware or route handler
         next();
     } catch (err) {
+        
+        // If token verification fails, log the error and return 403 Forbidden
         console.error('JWT Verification Error:', err);
         return res.sendStatus(403);
     }
