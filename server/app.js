dca602c804e477096c24f7d02acbed3a9f933a88
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 17:10:29 -0800

    cors fix for hosting

--- server/app.js
+++ server/app.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://bookforum-zog8.onrender.com',
+    credentials: true
+  }));
 
 const connectDB = async () => {
   try {
