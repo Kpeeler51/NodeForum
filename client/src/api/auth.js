63a6eaf3d48bd8ed0a11021acb4724ac5b93d0ee
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 18:20:05 -0800

    comments for auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,5 +1,7 @@
+// Base URL for API requests. Set inside of a .env file. falls back to localhost:3000.
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
+// Attempts to log the user in with the provided credentials.
 export const login = async (email, password) => {
     const response = await fetch(`${API_BASE_URL}/api/users/login`, {
       method: 'POST',
@@ -14,7 +16,7 @@ export const login = async (email, password) => {
     }
     return response.json();
   };
-  
+  // Attempts to register a new user with the provided credentials.
 export const register = async (username, email, password) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
       method: 'POST',
