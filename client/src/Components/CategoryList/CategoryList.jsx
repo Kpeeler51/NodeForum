a309a28f711dc2657ba64e6c82ec007c881d352a
Kyle Peeler <kylepeeler@yahoo.com>
2025-02-16 18:23:29 -0800

    categorylist comments

--- client/src/Components/CategoryList/CategoryList.jsx
+++ client/src/Components/CategoryList/CategoryList.jsx
@@ -1,17 +1,21 @@
+// Module Imports
 import PropTypes from 'prop-types';
 import './CategoryList.css';
 
+// Renders a list of categories, each category is a button to sort the thread list based on category.
 const CategoryList = ({ categories, selectedCategory, onSelectCategory }) => {
   return (
     <div className="category-list">
       <h3>Categories</h3>
       <div className="category-buttons">
+        {/* ALL button for displaying every category. */}
         <button 
           onClick={() => onSelectCategory(null)}
           className={selectedCategory === null ? 'active' : ''}
         >
           All
         </button>
+        {/* maps through the categories and creates a button for each. */}
         {categories.map((category) => (
           <button
             key={category._id}
@@ -26,6 +30,7 @@ const CategoryList = ({ categories, selectedCategory, onSelectCategory }) => {
   );
 };
 
+// PropTypes validation.
 CategoryList.propTypes = {
   categories: PropTypes.array.isRequired,
   selectedCategory: PropTypes.string,
